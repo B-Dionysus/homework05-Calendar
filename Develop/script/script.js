fdf6c039648b69f3e6da997c27e1e12241d89dfb
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-03 09:43:02 -0500

    Populated the ui

--- Develop/script/script.js
+++ Develop/script/script.js
@@ -1,27 +1,26 @@
-/* <div class="row">
-<div class="col-1">
-  <div class="hour-of-the-day">
-      11:00am
-  </div>
-</div>
-  <div class="col-10 event-column">
-      <input class="event-for-the-day">
-  </div>
-  <div class="col-1 save-column">
-    <button class="save-event">
-      <i class="far fa-save"></i>
-    </button>
-  </div>
-</div> */
+
 function init(){
-   $("#time-block-section").append(newTimeBlock(11));
+    for(i=9;i<=17;i++){
+        time=i;
+        if(time>12)time-=12;
+        thisId=time;
+        time=time.toString()+":00";
+        $("#time-block-section").append(newTimeBlock(time, thisId, thisDate).attr("id", thisId));
+    }
 }
 
-function newTimeBlock(time){
-
-
+function newTimeBlock(time,thisId, thisDate){
     var newBlock=$("<div>").addClass("row");
-    newBlock.append(($("<div>")).addClass("col-1"));
-    newBlock.firstChild().addClass("TEST");
+    newBlock.append(($("<div>")).addClass("col-2 time-of-day"));
+    newBlock.find('.time-of-day').append($("<div>").addClass("hour-of-the-day").text(time));
+
+    
+    newBlock.append(($("<div>")).addClass("col-9 event-column"));
+    newBlock.find('.event-column').append($("<input>").addClass('event-for-the-day'));
+
+    
+    newBlock.append(($("<div>")).addClass("col-1 save-column"));
+    newBlock.find('.save-column').append($("<button>").addClass('save-event').html('<i class="far fa-save"></i>'));
+    newBlock.find('.save-event').on("click",function(){addCalendarEvent(thisId, thisDate)});
     return newBlock;
 }
